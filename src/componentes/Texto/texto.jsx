8e68cd13065821293434d7e476bb6b536f337a14
jordidel170 <157019626+jordidel170@users.noreply.github.com>
2024-04-18 13:15:59 +0000

    added components

---
+++ src/componentes/Texto/texto.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from '/workspaces/landing-page-form-react/src/assets/react.svg'
+import viteLogo from '/workspaces/landing-page-form-react/public/vite.svg'
+import '/workspaces/landing-page-form-react/src/componentes/Texto/texto.css'
+
+function Texto() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and  xfgh sdfhsr 
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Melisaaaaaaaaaaaaaaaaaaa
+      </p>
+    </>
+  )
+}
+
+export default Texto
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Content from './componentes/Content/content.jsx'
+import NavBar from './componentes/Navbar/Navbar.jsx'
+import Texto from './componentes/Texto/texto.jsx'
+import Logo from './componentes/Logo/Logo.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Content />
+    <NavBar />
+    <Texto />
+    <Logo />
   </React.StrictMode>,
 )
