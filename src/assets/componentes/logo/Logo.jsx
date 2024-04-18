55dc53b553a3125fe4eb3ba416c3e62fef64ecb9
Rocío Martín <149075907+Rocio-fullstack@users.noreply.github.com>
2024-04-18 17:20:36 +0000

    logo add

--- src/assets/componentes/logo/Logo.jsx
+++ src/assets/componentes/logo/Logo.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import App from '../../../App'
 import './Logo.css';
+import Logo2 from './logo2.png'
 export const Logo = () => {
     return (
 
         <>    
-                <h4>Logo Aquí</h4>
+           <img className='foto-img' src={Logo2}/>
+                <h1>AJRM Team</h1>
         </>
     )
 };
